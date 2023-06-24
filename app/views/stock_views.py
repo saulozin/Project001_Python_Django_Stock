30e109678c23cd00b8bcf2c03f18603a8e40c84e
Saulo Maia <saulomaia2@gmail.com>
2023-06-23 22:49:41 -0400

    Mudando o numero de itens por pagina do stock para 10

--- app/views/stock_views.py
+++ app/views/stock_views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 def stock(request):
     products = Product.objects.all().order_by('-id')
 
-    paginator = Paginator(products, 6)
+    paginator = Paginator(products, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
